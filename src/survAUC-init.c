302f818295e6bebf744ffba9917dbf289406825e
Nan Xiao <me@nanx.me>
2022-05-16 22:20:04 -0400

    Fix --as-cran check note

--- src/survAUC-init.c
+++ src/survAUC-init.c
@@ -1,14 +1,34 @@
+// Generated with tools::package_native_routine_registration_skeleton()
+
 #include <R.h>
 #include <Rinternals.h>
-
-#include "survAUC_Cham_Diao.h"
-#include "survAUC_SongZhou.h"
-#include "survAUC_UNO.h"
-
+#include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-void R_init_survAUC(DllInfo *info) {
-  R_RegisterCCallable("survAUC", "Cham_Diao",  (DL_FUNC) &Cham_Diao);
-  R_RegisterCCallable("survAUC", "auc_SZ",  (DL_FUNC) &auc_SZ);
-  R_RegisterCCallable("survAUC", "auc_uno",  (DL_FUNC) &auc_uno);
+/* FIXME:
+ Check these declarations against the C/Fortran source code.
+ */
+
+/* .C calls */
+extern void auc_uno(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+/* .Call calls */
+extern SEXP auc_SZ(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP Cham_Diao(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+
+static const R_CMethodDef CEntries[] = {
+  {"auc_uno", (DL_FUNC) &auc_uno, 15},
+  {NULL, NULL, 0}
+};
+
+static const R_CallMethodDef CallEntries[] = {
+  {"auc_SZ",    (DL_FUNC) &auc_SZ,    13},
+  {"Cham_Diao", (DL_FUNC) &Cham_Diao, 11},
+  {NULL, NULL, 0}
+};
+
+void R_init_survAUC(DllInfo *dll)
+{
+  R_registerRoutines(dll, CEntries, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
 }
